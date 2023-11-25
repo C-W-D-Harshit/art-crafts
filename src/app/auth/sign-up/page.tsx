8ea4e4026226519e1caacf8ac17873a23e354886
Harshit Sharma <cwd.harshit911@gmail.com>
2023-11-25 18:31:29 +0530

    fixed last commit

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -3,14 +3,6 @@ import SignUpForm from "@/components/pages/sign-up/SignUpForm";
 import React from "react";
 
 export default function page() {
-  const defaultOptions = {
-    loop: true,
-    autoplay: true,
-    //   animationData: animationData,
-    rendererSettings: {
-      preserveAspectRatio: "xMidYMid slice",
-    },
-  };
   return (
     <div className="min-h-[calc(100dvh-8rem)] md:min-h-[calc(100dvh-4rem)] w-full flex">
       <div className="w-[60%] bg-slate-100 relative hidden md:flex "></div>
